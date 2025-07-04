e387fbe1a68a370b432568ebf2a34440729bbab6
Vale Moreno <75440635+valemorenodev@users.noreply.github.com>
2025-07-03 23:42:43 -0500

    test: Configuracion de jest base para creacion de pruebas unitarias

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      useESM: true,
+      tsconfig: 'tsconfig.app.json'
+    }]
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  extensionsToTreatAsEsm: ['.ts', '.tsx']
+};
