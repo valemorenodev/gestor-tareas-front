5a58f495c8e97d2b46ed9c5ed4fb56b51a043268
Vale Moreno <75440635+valemorenodev@users.noreply.github.com>
2025-07-03 22:09:40 -0500

    Ajustar responsive

--- src/components/TaskItem/TaskItem.tsx
+++ src/components/TaskItem/TaskItem.tsx
@@ -1,4 +1,5 @@
 import { type Task } from '../../types/Task';
+import './TaskItem.css'
 
 interface Props {
   task: Task;
@@ -9,13 +10,13 @@ interface Props {
 
 const TaskItem = ({ task, onToggle, onDelete, onEdit }: Props) => {
   return (
-    <li style={{ color: task.color }}>
+    <li style={{ color: task.color }} className='task-item'>
       <input
         type="checkbox"
         checked={task.state === 'terminada'}
         onChange={() => onToggle(task._id!, task.state !== 'terminada')}
       />
-      <strong>{task.title}</strong> - {task.description}
+      <span><strong>{task.title}</strong><p> - {task.description}</p></span>
       <button onClick={() => onEdit(task)}>✏️ Editar</button>
       <button onClick={() => onDelete(task._id!)}>🗑️ Eliminar</button>
     </li>
